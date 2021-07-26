da32d5254799a51203f1fc9ddbf82d59abd1bde7
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-07-26 13:15:49 -0400

    added files

---
+++ 103/practice/script.js
@@ -0,0 +1,24 @@
+console.log("Hello");
+
+var userName = "Angel";
+var userAge=28;
+var student=true;
+
+console.log(userName);
+console.log(userAge);
+console.log(student);
+
+// document.write(userName);
+// document.write(userAge);
+// document.write(student);
+
+// console.log(`
+// <p> <b>Name: </b> ${userName} </p>
+// <p> <b>Age: </b> ${userAge} </p>
+// `);
+
+
+document.write(`
+<p> <b>Name: </b> ${userName} </p>
+<p> <b>Age: </b> ${userAge} </p>
+`);
