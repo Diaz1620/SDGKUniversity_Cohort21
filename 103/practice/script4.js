739ff6b117b1068db611048e38253b97c16b65c2
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-07-29 20:53:24 -0400

    added file

---
+++ 103/practice/script4.js
@@ -0,0 +1,25 @@
+let band="Metallica", 
+    song="Enter sandman",
+    text;
+
+
+text=band+": "+song;
+text=text.concat(" " , "it's great!")
+console.log(text);
+console.log(text.length);
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+
+
+let activity="I am learning JavaScript";
+text=activity.split(" ");
+let hobbies="play videogames, read, watch tv, listen music";
+text=hobbies.split(",")
+console.log(text);
+
+let email="kenny@gmail.com";
+text=email.includes("@");
+console.log(text);
+
+
+
---
+++ 103/practice/script5.js
@@ -0,0 +1,8 @@
+let result;
+
+result=Math.round(2.5);
+console.log(result);
+result=Math.floor(2.99);
+console.log(result);
+result=Math.ceil(2.01);
+console.log(result);
