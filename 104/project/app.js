ca37f22d58e42cde849bdb846b1e48ee871758d5
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-08-03 22:07:36 -0400

    added new folders/files

---
+++ 104/project/app.js
@@ -0,0 +1,97 @@
+var salon={
+    name:"The Fashion Pet",
+    telephone: "555-555-5555",
+    address:{
+        street:"Palm",
+        number:"345",
+        city:"Santa Monica",
+        state:"CA"
+    },
+    hour:{
+        open:"9:00 am",
+        close:"5:00 pm"
+    },
+    pets:[
+        {   name:"Scooby",
+            age:60,
+            gender:"Male",
+            breed:"Dane",
+            service:"Full Service",
+            ownerName:"Shaggy",
+            contactPhone:"555-555-5555"
+        },
+        {   name:"Scrappy",
+            age:50,
+            gender:"Male",
+            breed:"Dane",
+            service:"Nails Cut",
+            ownerName:"Shaggy",
+            contactPhone:"555-555-5555"
+        },
+        {   name:"Tweety",
+            age:20,
+            gender:"Female",
+            breed:"Bird",
+            service:"Fluff Feathers",
+            ownerName:"Old Lady",
+            contactPhone:"555-555-5554"
+        },
+        {   name:"Monty",
+            age:10,
+            gender:"Male",
+            breed:"Python",
+            service:"De-Fang",
+            ownerName:"Person",
+            contactPhone:"555-555-3333"
+        }
+    ]
+}
+
+// var scoopy={
+//     name:"Scooby",
+//     age:60,
+//     gender:"Male",
+//     breed:"Dane",
+//     service:"Full Service",
+//     ownerName:"Shaggy",
+//     contactPhone:"555-555-5555"
+// }
+
+
+function displayInfo(){
+    document.getElementById("footer-site").innerHTML=`
+    <p>${salon.name}</p>
+    <p>${salon.address.number} ${salon.address.street}, ${salon.address.city}, ${salon.address.state}, ${salon.telephone}</p>
+    <p>The salon is open from ${salon.hour.open} to ${salon.hour.close}</p>
+    `;
+}
+
+displayInfo();
+
+
+
+
+
+
+
+
+// console.log("help");
+// // object literal
+// const person={
+//     name: "Angel",
+//     city: "New York",
+//     age: 27,
+//     student: true,
+//     music:['Coldplay',"Oasis","Beatles"],
+//     address:{
+//         street: "University",
+//         number:"262-K"
+//     }
+// }
+// console.log(person);
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.city);
+// console.log(person.music[1]);
+// console.log(person.address.street);
+
