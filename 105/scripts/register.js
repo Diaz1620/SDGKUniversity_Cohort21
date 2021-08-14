ae2d4549833fa269fe494f16f70d0e259347ce04
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-08-13 23:06:03 -0400

    added new files and code

---
+++ 105/scripts/register.js
@@ -0,0 +1,41 @@
+console.log("Users register");
+class User {
+    constructor(firstName, lastName, username, email, pass, address, address2, city, state, zip, age, method) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = pass;
+        this.address = address;
+        this.address2 = address2;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.age = age;
+        this.method = method;
+    }
+}
+
+function registerUser(){
+    let firstName =$("#inputfirstName").val();
+    let lastName =$("#inputlastName").val();
+    let username =$("#inputUsername").val();
+    let email = $("#inputEmail").val();
+    let pass = $("#inputPassword").val();
+    let address = $("#inputAddress").val();
+    let address2 = $("#inputAddress2").val();
+    let city = $("#inputCity").val();
+    let state = $("#inputState").val();
+    let zip = $("#inputZip").val();
+    let age = $("#inputAge").val();
+    let method = $("#inputPaymentMethod").val();
+    let user = new User(firstName,lastName,username,email,pass,address,address2,city,state,zip,age,method)
+    console.log(user);
+}
+
+
+function init(){
+    console.log("init function");
+    $("#btnSave").click(registerUser);
+}
+window.onload=init;
