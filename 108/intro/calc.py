5b3a57c564575f437f8c7a9050c0cc1013e01ea5
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-09-10 01:47:41 -0400

    add py

---
+++ 108/intro/calc.py
@@ -0,0 +1,36 @@
+"""
+author: Yadiel Diaz
+system: simple python calc
+"""
+
+#imports
+from display import print_menu, print_header
+
+#global vars
+
+#direct instructions
+print_header()
+print_menu()
+
+option = input("Select an option: ")
+
+num1 = float(input("Please provide num1: "))
+num2 = float(input("Please provide num2: "))
+
+if option == "1":
+    res = num1 + num2
+
+elif option == "2":
+    res = num1 - num2
+
+elif option == "3":
+    res = num1 * num2
+
+elif option == "4":
+    if num2 == 0:
+        print("Error: Zero division is not allowed")
+        res = "Error"
+    else:
+        res = num1/num2
+
+print(f"The result is: {res}")
