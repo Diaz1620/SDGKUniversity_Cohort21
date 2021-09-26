98176639a03fcf9f234280e902aed69f72f251a6
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-09-26 14:51:32 -0400

    new commit

--- 108/flask_api/server.py
+++ 108/flask_api/server.py
@@ -1,9 +1,11 @@
 import json
 from flask import Flask, render_template, abort, request
 from flask.globals import request
+from flask_cors import CORS
+from mock_data import mock_data
 
 app = Flask(__name__)
-from mock_data import mock_data
+CORS(app)
 
 me = {
     "name": "Yadiel",
@@ -45,6 +47,10 @@ def save_product():
     product = request.get_json()
     if not "price" in product or product["price"] <= 0:
         abort(400,"Price is required and should be greater that zero")
+
+    if not "title" in product or len(product["title"]) < 5:
+        abort(400, "Title is required and should be at least 5 chars long")
+        
     mock_data.append(product)
     product["_id"] = len(product["title"])
     return json.dumps(product)
@@ -67,7 +73,7 @@ def get_by_id(id):
         if not found:
             abort(404)
 
-@app.route("api/catalog/<cat>")
+@app.route("/api/catalog/<cat>")
 def get_by_category(cat):
     found = False
     categories = []
